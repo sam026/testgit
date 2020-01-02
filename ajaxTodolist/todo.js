1b1a582e9bdfae9899f7d1a6c2fc768ad92293b8
kaito26 <kaitosan268@gmail.com>
2020-01-02 18:06:17 +0630

    second add test vscode

---
+++ ajaxTodolist/todo.js
@@ -0,0 +1,23 @@
+<script>
+
+function getTodoData()
+ {
+	var request = new XMLHttpRequest();
+	request.open("GET", "todo.json");
+	request.onreadystatechange = function() {
+	var listDiv = document.getElementById("todoList");
+	if (this.readyState == this.DONE && this.status == 200) {
+	if (this.responseText)
+		{
+			listDiv.innerHTML = this.responseText;
+		}
+		else
+		{
+			console.log("Error: Data is empty");
+		}
+	}
+}
+request.send();
+}
+
+</script>
